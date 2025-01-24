f39f51c65498c21bbde7308ba3273643990efdc8
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-01-25 01:49:52 +0530

    section:39

--- models/campground.js
+++ models/campground.js
@@ -1,29 +1,17 @@
 const mongoose = require('mongoose');
-//const review = require('./review');
+
 const Schema = mongoose.Schema;
 
 
 const CampgroundSchema = new Schema({
     title:String,
-    image:String,
-    price:Number,
+    price:String,
     description:String,
     location:String,
-    // reviews:[
-    //     {
-    //         type:Schema.Types.ObjectId,
-    //         ref:'Review'
-    //     }
-    // ]
+
 });
 
-// CampgroundSchema.post('findOneAndDelete', async function(doc){
-//     if(doc){
-//         await review.remove({
-//             _in: doc.reviews
-//         })
-//     }
-// })
+
 
 module.exports = mongoose.model('Campground',CampgroundSchema);
 
---
+++ seeds/seedHelpers.js
@@ -0,0 +1,17 @@
+module.exports.descriptors = [
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Cascade'
+]
+
+    module.exports.places = [
+    'Flats',
+    'Village',
+    'Canyon',
+    'Pond',
+    'Group Camp',
+    'Horse Camp',
+    'Hollow'
+]
