787ae0ea6dbd0537a97c84706569790ea29e1699
Nikunj Singla <nikunjsingla2005@gmail.com>
2025-01-25 17:55:51 +0530

    added the reviews model

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const review = require('./review');
 
 const Schema = mongoose.Schema;
 
@@ -9,8 +10,25 @@ const CampgroundSchema = new Schema({
     price:Number,
     description:String,
     location:String,
+    reviews:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Review'
+        }
+    ]
 
 });
+
+
+CampgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 
 
